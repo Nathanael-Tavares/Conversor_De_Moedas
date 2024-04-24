ca24bf1f067e49bc95d7d8229b9d61d576e931cb
Nathanael Tavares Alves <nathan.nta14@gmail.com>
2024-04-24 09:13:42 -0300

    Projeto finalizado e funcionando

--- src/model/FetchCurrencyPair.java
+++ src/model/FetchCurrencyPair.java
@@ -8,10 +8,10 @@ public class FetchCurrencyPair {
         Map<Integer, String[]> map = new HashMap<>();
         map.put(1, new String[]{"USD", "BRL"});
         map.put(2, new String[]{"BRL", "EUR"});
-        map.put(3, new String[]{"EUR", "BRL"});
+        map.put(3, new String[]{"GBP", "BRL"});
         map.put(4, new String[]{"BRL", "JPY"});
         map.put(5, new String[]{"BRL", "USD"});
-        map.put(6, new String[]{"EUR", "USD"});
+        map.put(6, new String[]{"EUR", "CAD"});
         return map.get(key);
     }
 }
